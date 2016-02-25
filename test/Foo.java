244eec20c95a2910eb1c7953675ff85ece8c6bfa
laboon <laboon@gmail.com>
2016-02-25 16:48:34 -0500

    added birds and noogies

---
+++ test/Foo.java
@@ -0,0 +1,8 @@
+public class Foo {
+
+    public static void main(String[] args) {
+        System.out.println("Monkeys!");
+	System.out.println("Birds!");
+	System.out.println("Noogies!");
+    }
+}
